7dcccf73b8d65d0d028f76b6a001ba76c2ae3a85
Flyon <flyon314@gmail.com>
2025-05-21 14:55:07 +0900

    [Bronze V] Title: Hello World, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2557. Hello World/Hello World.cc
+++ 백준/Bronze/2557. Hello World/Hello World.cc
@@ -1,7 +1,16 @@
+#include <iosfwd>
 #include <iostream>
-
+#include <stdio.h>
+#include <string>
 using namespace std;
 
-int main() {
-    cout<<"Hello World!";
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+
+     cout << "Hello World!" << "\n";
+    return 0;
 }
